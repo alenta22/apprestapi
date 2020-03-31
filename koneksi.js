5c5af68177543c168d3e59b36fe7bf49cfdc3b96
alen <alenbio42@gmail.com>
2020-03-31 14:10:30 +0700

    membuat server.js

---
+++ koneksi.js
@@ -0,0 +1,11 @@
+var mysql = require('mysql');
+
+//buat koneksi database
+const conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'dbrestapi',
+});
+
+conn, connect
